02f79f027df4e9d33f2cd5a3decf6c56dafc5bf4
JEAGNT96 <98776876+JEAGNT96@users.noreply.github.com>
2025-04-04 13:18:07 -0700

    Update index.js

--- 07-Modulos/index.js
+++ 07-Modulos/index.js
@@ -1,6 +1,6 @@
 /* Importamos la funcion al archivo principal */
 import verificarEmail from "./function.js";
 /* Enviamos el parametro email*/
-let email = ("hola");
+let email = ("eduardoag2003@outlook.com");
 let resultado = verificarEmail(email); 
 console.log(resultado);
